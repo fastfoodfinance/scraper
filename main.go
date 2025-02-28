62063d4aab0f8d317f97023f6868930ecacf6dee
Philippe Vinchon <p.vinchon@gmail.com>
2025-02-28 14:11:46 +0000

    Write menus to file

--- main.go
+++ main.go
@@ -3,20 +3,35 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/fastfoodfinance/scraper/internal"
 	"github.com/fastfoodfinance/scraper/internal/ubereats"
 )
 
+func writeFile(menus []*internal.Menu) {
+	json, err := json.Marshal(menus)
+	if err != nil {
+		log.Fatalln("failed to marshal json", err)
+	}
+
+	outputDir := "output"
+	err = os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		log.Fatalln("failed to create directory", err)
+	}
+
+	outputFile := filepath.Join(outputDir, "menus.json")
+	err = os.WriteFile(outputFile, json, os.ModePerm)
+	if err != nil {
+		log.Fatalln("failed to write to file", err)
+	}
+}
+
 func main() {
 	menus := []*internal.Menu{}
 	menus = append(menus, ubereats.Menus()...)
 
-	json, err := json.Marshal(menus)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	str := string(json)
-	log.Println(str)
+	writeFile(menus)
 }
